315a47d8d6ef64dc0687c0fed0961571a45a991e
bikashmachahary <92855747+bikashmachahary@users.noreply.github.com>
2021-10-22 13:06:16 +0530

    For termux biganners
    This script will install all ur termux requirements.if u are biganners then u can use this script

---
+++ biganners.sh
@@ -0,0 +1,11 @@
+pkg update && pkg upgrade -y
+pkg install git 
+pkg install python -y
+pkg install python2 -y
+pkg install lolcat
+pkg install cmatrix
+pkg install openssh -y
+pkg install ruby
+pkg install figlet
+pkg install root-repo
+figlet THANKS FOR USING 
